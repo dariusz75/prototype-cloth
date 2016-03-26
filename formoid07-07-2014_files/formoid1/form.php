ab2081168113c380c89432e2323aff8b099f6a3b
Dariusz Franczak <darius32@op.pl>
2016-03-26 20:01:54 +0000

    uploaded to github

---
+++ formoid07-07-2014_files/formoid1/form.php
@@ -0,0 +1,32 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', 'darius32@op.pl');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Thanks for filling out my form!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+require_once str_replace('\\', '/', __DIR__) . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?=dirname($form_path)?>/formoid-solid-blue.css" type="text/css" />
+<span class="alert alert-success"><?=FINISH_MESSAGE;?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?=dirname($form_path)?>/formoid-solid-blue.css" type="text/css" />
+<script type="text/javascript" src="<?=dirname($form_path)?>/jquery.min.js"></script>
+<form enctype="multipart/form-data" class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:280px;min-width:150px" method="post"><div class="title"><h2>My form</h2></div>
+	<div class="element-name<?frmd_add_class("name")?>"><label class="title"></label><span class="nameFirst"><input placeholder="Name First" type="text" size="8" name="name[first]" /><span class="icon-place"></span></span><span class="nameLast"><input placeholder="Name Last" type="text" size="14" name="name[last]" /><span class="icon-place"></span></span></div>
+	<div class="element-phone<?frmd_add_class("phone")?>"><label class="title"></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" placeholder="Phone" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-email<?frmd_add_class("email")?>"><label class="title"></label><div class="item-cont"><input class="large" type="email" name="email" value="" placeholder="Email"/><span class="icon-place"></span></div></div>
+	<div class="element-file<?frmd_add_class("file")?>"><label class="title"></label><div class="item-cont"><label class="large" ><div class="button">Your CV</div><input type="file" class="file_input" name="file" /><div class="file_text">No file selected</div><span class="icon-place"></span></label></div></div>
+	<div class="element-textarea<?frmd_add_class("textarea")?>"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" placeholder="Message"></textarea><span class="icon-place"></span></div></div>
+<div class="submit"><input type="submit" value="Submit"/></div></form><script type="text/javascript" src="<?=dirname($form_path)?>/formoid-solid-blue.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
